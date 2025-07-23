818b5b71a4cba530dcbfbb06f528d2c4551761a8
oldsandiaz <calebdiazccs@gmail.com>
2025-07-23 14:21:30 -0500

    bash script to run all scripts

---
+++ run_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script runs the R scripts in the correct order.
+
+Rscript code/00_renv_restore.R
+Rscript code/0a_respiratory_support_waterfall.R
+Rscript code/01_cohort_identification.R
+Rscript code/02_feature_set_processing.R
+Rscript code/03_table1.R
+Rscript code/04_model_training.R
